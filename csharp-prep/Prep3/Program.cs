dbd158683ed0e8f7b0b23918373758d246d2d7ab
jaywendt <jaywendt1993@gmail.com>
2023-09-16 20:01:51 -0500

    #3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random ranGen = new Random();
+        int magNum = ranGen.Next(1, 100);
+
+        int guess = -1;
+
+        while (guess != magNum)
+            {
+                Console.Write("Guess my magic number! ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > magNum)
+                {
+                    Console.WriteLine("Lower!");
+                }
+                else if (guess < magNum)
+                {
+                    Console.WriteLine("Higher!");
+                }
+                else
+                {
+                    Console.Write("You guessed it!");
+                }
+            }
     }
 }
