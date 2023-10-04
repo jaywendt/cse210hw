07c7821c87e284e697631edf9851e27024bab37c
jaywendt <jaywendt1993@gmail.com>
2023-10-04 11:54:49 -0500

    prove4-1

---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class ListingActivity : Activity
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+
+    public void Run()
+    {
+
+    }
+
+    public string GetRandomPrompt()
+    {
+
+    }
+
+    public List<string> GetListFromUser()
+    {
+
+    }
+}
---
+++ prove/Develop04/ReflectingActivity.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class ReflectingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts;
+
+    public void Run()
+    {
+
+    }
+
+    public string GetRandomPrompt()
+    {
+
+    }
+
+    public string GetRandomQuestion()
+    {
+
+    }
+
+    public void DisplayPrompt()
+    {
+
+    }
+
+    public void DisplayQuestions()
+    {
+
+    }
+}
