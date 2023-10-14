9862e76ae6929e50791a6d30e06a59d6d9b95b3d
jaywendt <jaywendt1993@gmail.com>
2023-10-13 19:04:43 -0500

    poly1

---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class EternalGoal : Goal
+{
+    public EternalGoal(string name, string description, int points) : base ("Eternal Goal")
+    {
+
+    }
+
+    public void RecordEvent()
+    {
+
+    }
+
+    public bool IsComplete()
+    {
+
+    }
+
+    public string GetStringRepresentation()
+    {
+        
+    }
+}
